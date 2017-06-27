ab814cdf50d52342a78f2530c422ef137ac8c5a0
JIA LIU <timspace1988@hotmail.com>
2017-06-27 22:12:53 +1000

    Set default header image for heroku

--- config/blog.php
+++ config/blog.php
@@ -5,7 +5,7 @@ return [
     'subtitle' => 'A clean blog written in Laravel 5.1',
     'description' => 'This is my meta description',
     'author' => 'Jia Liu',
-    'page_image' => 'https://s3.us-east-2.amazonaws.com/jiablog/Header%20Images/home-bg.jpg',
+    'page_image' => 'https://s3.us-east-2.amazonaws.com/jiablogheroku/header_images/home-bg.jpg',
     'posts_per_page' => 10,
     'uploads' => [
         // 'storage' => 'local',
